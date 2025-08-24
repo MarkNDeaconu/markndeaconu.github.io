9a007de1447a0195deed21cee6d077e462d121ee
Mark <mark.deaconu@gmail.com>
2025-08-23 22:56:14 -0400

    added my face to the home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,80 +8,117 @@ import { Posts } from "@/components/blog/Posts";
 
 export default function Home() {
   return (
-    <Column maxWidth="m" gap="xl" horizontal="center">
-      <Schema
-        as="webPage"
-        baseURL={baseURL}
-        path={home.path}
-        title={home.title}
-        description={home.description}
-        image={`/api/og/generate?title=${encodeURIComponent(home.title)}`}
-        author={{
-          name: person.name,
-          url: `${baseURL}${about.path}`,
-          image: `${baseURL}${person.avatar}`,
-        }}
-      />
-      <Column fillWidth paddingY="24" gap="m">
-        <Column maxWidth="s">
-          {home.featured.display && (
-          <RevealFx fillWidth horizontal="start" paddingTop="16" paddingBottom="32" paddingLeft="12">
-            <Badge background="brand-alpha-weak" paddingX="12" paddingY="4" onBackground="neutral-strong" textVariant="label-default-s" arrow={false}
-              href={"/work/cordax"}>
-              <Row paddingY="2">{'Recent Project: Cordax'}</Row>
-            </Badge>
-          </RevealFx>
-          )}
-          <RevealFx translateY="4" fillWidth horizontal="start" paddingBottom="16">
-            <Heading wrap="balance" variant="display-strong-l">
-              {home.headline}
-            </Heading>
-          </RevealFx>
-          <RevealFx translateY="8" delay={0.2} fillWidth horizontal="start" paddingBottom="32">
-            <Text wrap="balance" onBackground="neutral-weak" variant="heading-default-xl">
-              {home.subline}
-            </Text>
-          </RevealFx>
-          <RevealFx paddingTop="12" delay={0.4} horizontal="start" paddingLeft="12">
-            <Button
-              id="about"
-              data-border="rounded"
-              href={about.path}
-              variant="secondary"
-              size="m"
-              weight="default"
-              arrowIcon
-            >
-              <Flex gap="8" vertical="center" paddingRight="4">
-                {about.avatar.display && (
-                  <Avatar
-                    marginRight="8"
-                    style={{ marginLeft: "-0.75rem" }}
-                    src={person.avatar}
-                    size="m"
-                  />
-                )}
-                {about.title}
-              </Flex>
-            </Button>
-          </RevealFx>
-        </Column>
-      </Column>
-      <RevealFx translateY="16" delay={0.6}>
-        <Projects range={[1, 1]} />
-      </RevealFx>
-      {routes["/blog"] && (
-        <Flex fillWidth gap="24" mobileDirection="column">
-          <Flex flex={1} paddingLeft="l" paddingTop="24">
-            <Heading as="h2" variant="display-strong-xs" wrap="balance">
-              Latest from the blog
-            </Heading>
-          </Flex>
-          <Flex flex={3} paddingX="20">
-            <Posts range={[1, 2]} columns="2" />
-          </Flex>
-        </Flex>
+<Column maxWidth="m" gap="xl" horizontal="center">
+  <Schema
+    as="webPage"
+    baseURL={baseURL}
+    path={home.path}
+    title={home.title}
+    description={home.description}
+    image={`/api/og/generate?title=${encodeURIComponent(home.title)}`}
+    author={{
+      name: person.name,
+      url: `${baseURL}${about.path}`,
+      image: `${baseURL}${person.avatar}`,
+    }}
+  />
+
+  {/* Flex wrapper for text + image */}
+  <Flex fillWidth gap="24" mobileDirection="column">
+    
+    {/* Text column */}
+    <Column flex={3}>
+      {home.featured.display && (
+        <RevealFx fillWidth horizontal="start" paddingTop="16" paddingBottom="32" paddingLeft="12">
+          <Badge
+            background="brand-alpha-weak"
+            paddingX="12"
+            paddingY="4"
+            onBackground="neutral-strong"
+            textVariant="label-default-s"
+            arrow={false}
+            href={"/work/cordax"}
+          >
+            <Row paddingY="2">{'Recent Project: Cordax'}</Row>
+          </Badge>
+        </RevealFx>
       )}
+      <RevealFx translateY="4" fillWidth horizontal="start" paddingBottom="16">
+        <Heading wrap="balance" variant="display-strong-l">
+          {home.headline}
+        </Heading>
+      </RevealFx>
+      <RevealFx translateY="8" delay={0.2} fillWidth horizontal="start" paddingBottom="32">
+        <Text wrap="balance" onBackground="neutral-weak" variant="heading-default-xl">
+          {home.subline}
+        </Text>
+      </RevealFx>
+      <RevealFx paddingTop="12" delay={0.4} horizontal="start" paddingLeft="12">
+        <Button
+          id="about"
+          data-border="rounded"
+          href={about.path}
+          variant="secondary"
+          size="m"
+          weight="default"
+          arrowIcon
+        >
+          <Flex gap="8" vertical="center" paddingRight="4">
+            {about.avatar.display && (
+              <Avatar
+                marginRight="8"
+                style={{ marginLeft: "-0.75rem" }}
+                src={person.avatar}
+                size="m"
+              />
+            )}
+            {about.title}
+          </Flex>
+        </Button>
+      </RevealFx>
     </Column>
+
+    {/* Image column */}
+    <Column flex={1} horizontal="center">
+      <RevealFx translateY="12" delay={0.6} horizontal="end">
+        <Flex style={{ width: "350px", height: "350px" }}>
+          <img
+            src="/images/markwbots.png"
+            alt="My photo"
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              borderRadius: "20px",
+              opacity: "1"
+            }}
+          />
+        </Flex>
+      </RevealFx>
+    </Column>
+  </Flex>
+
+  {/* Blog section */}
+  <RevealFx delay={0.6}>
+    {routes["/blog"] && (
+      <Flex fillWidth gap="24" mobileDirection="column">
+        <Flex flex={1} paddingLeft="l" paddingTop="24">
+          <Heading as="h2" variant="display-strong-xs" wrap="balance">
+            Latest from my blog
+          </Heading>
+        </Flex>
+        <Flex flex={3} paddingX="20">
+          <Posts range={[1, 2]} columns="2" />
+        </Flex>
+      </Flex>
+    )}
+  </RevealFx>
+
+  {/* Projects section */}
+  {/* <RevealFx translateY="16" delay={0.6}>
+    <Projects range={[1, 1]} />
+  </RevealFx> */}
+</Column>
+
   );
 }
